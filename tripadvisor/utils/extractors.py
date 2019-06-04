e49c6daa045461f35ff223330ecc0d4cae48761f
Antonio Carlos de Souza Zechin <antonio.zechin@b2wdigital.com>
2019-06-04 14:54:47 -0300

    metodo contains xpath para titulo e link

--- tripadvisor/utils/extractors.py
+++ tripadvisor/utils/extractors.py
@@ -5,7 +5,7 @@ def extract_content(text, selector):
     value = Selector(text=text).xpath(selector).extract()
     if value:
         return value[0]
-    return "vazio"
+    return ""
 
 def extract_list(text, selector):
     value = Selector(text=text).xpath(selector).extract()
